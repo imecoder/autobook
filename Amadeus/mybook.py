9aa1e6e9cc100af945f5f5e3e3cbc63cfa8bb5ff
liruigang <yueyemingming@163.com>
2021-11-20 22:55:47 +0800

    突破了textare输入及å触发的相关问题

--- Amadeus/mybook.py
+++ Amadeus/mybook.py
@@ -1,13 +1,17 @@
 import time
 import threadpool
+from selenium.webdriver.common.keys import Keys
+
 from mylog import *
 import myfile
 import json
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException, JavascriptException
 
 website = 'https://www.sellingplatformconnect.amadeus.com/LoginService/login.jsp?SITE=LOGINURL&LANGUAGE=GB'
+# website = 'D://mywork//autobook//Amadeus//test.html'
+
 
 ret, base_config = myfile.get_config("config.base.json")
 if ret == False :
@@ -17,8 +21,15 @@ ret, book_config_list = myfile.get_config("config.book.json")
 if ret == False :
     exit(0)
 
-browser = webdriver.Chrome()
-browser.get(website)
+try :
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("debuggerAddress", "127.0.0.1:6001")
+    browser = webdriver.Chrome(options=options)
+    browser.get(website)
+    logger.warning('title = ' + browser.title)
+except WebDriverException as e :
+    logger.warning('发生错误 = ' + str(e) )
+    exit(-1)
 
 def limit():
     # 2021-11-20 00:00 此处分发给员工时， 可以自行修改， 修改后编译即可
@@ -28,119 +39,90 @@ def limit():
         return True
     return False
 
-def wait_element(element, string) :
-    while True :
-        time.sleep(1)
+def wait_fun(element, string) :
+    time.sleep(1)
 
-        try:
-            browser.find_element_by_xpath("//*[@id='w2_firstInput']/span/input")
-        except NoSuchElementException as e:
-            print('等待加载' + string + '页面')
-            continue
-        break
+    try:
+        logger.warning('element = ' + element)
+        browser.find_element_by_xpath(element)
+    except NoSuchElementException as e:
+        logger.warning('等待加载 ' + string)
+        return False
 
-    print('登录' + string + '加载完成')
+    return True
+
+def wait_element(element, string, times) :
+    if times == 0 :
+        while not wait_fun(element, string):
+            pass
+
+        logger.warning(string + ' 加载完成')
+        return
+
+    for i in range(times) :
+        if wait_fun(element, string) == True :
+            logger.warning(string + ' 加载完成')
+            break
+
+    logger.warning(string + ' 未能加载完成')
+
 
 def login() :
 
-    wait_element("//*[@id='w2_firstInput']/span/input", '登录')
+    element = '//*[@id="w1_firstInput"]/span/input'
+    wait_element(element, '登录页面', 0)
+    browser.find_element_by_xpath(element).clear()
+    browser.find_element_by_xpath(element).send_keys('WXIAONAN')
 
-    browser.find_element_by_xpath("//*[@id='cookiebanner_cookiebanner_cookieBannerAcceptButton']/button").click()
-    browser.find_element_by_xpath("//*[@id='w2_firstInput']/span/input").send_keys('WXIAONAN')
-    browser.find_element_by_xpath("//*[@id='w2_officeId']/span/input").send_keys('LOSN82312')
-    browser.find_element_by_xpath("//*[@id='w2_passwordInput']/span/input").send_keys('Tut@2020')
+    element = '//*[@id="w1_officeId"]/span/input'
+    wait_element(element, '登录页面', 0)
+    browser.find_element_by_xpath(element).clear()
+    browser.find_element_by_xpath(element).send_keys('LOSN82312')
 
-# time.sleep(1)
-# browser.find_element_by_xpath("//*[@id='w7']/button/span/span[2]/span").click()
-# time.sleep(4)
-# browser.find_element_by_xpath("//*[@id='w7']/button/span/span[2]/span").click()
-# print('click over')
-#
-#
-# for i in range(10) :
-#     time.sleep(1)
-#     try:
-#         element = browser.find_element_by_xpath("//*[@id='w2_otpInput']/label")
-#     except NoSuchElementException as e:
-#         print('等待出现一次性密码')
-#         continue
-#
-#     print('得到结果 = ', element.text)
-#     onecode = input('请输入一次性密码 ：')
-#     break
-#
-# if onecode != '' :
-#     browser.find_element_by_xpath("//*[@id='w2_firstInput']/span/input").send_keys('WXIAONAN')
-#     browser.find_element_by_xpath("//*[@id='w2_officeId']/span/input").send_keys('LOSN82312')
-#     browser.find_element_by_xpath("//*[@id='w2_passwordInput']/span/input").send_keys('Tut@2020')
-#     browser.find_element_by_xpath("//*[@id='w2_otpInput']/span/input").send_keys(onecode)
-#
-#
-# time.sleep(1)
-# browser.find_element_by_xpath("//*[@id='w7']/button/span/span[2]/span").click()
-# time.sleep(4)
-# browser.find_element_by_xpath("//*[@id='w7']/button/span/span[2]/span").click()
-# print('click over')
-#
-#
-#
+    element = '//*[@id="w1_passwordInput"]/span/input'
+    wait_element(element, '登录页面', 0)
+    browser.find_element_by_xpath(element).clear()
+    browser.find_element_by_xpath(element).send_keys('Tut@2020')
 
-def start_cmd_page() :
-    while True :
+
+def start_cmd_page(size) :
+    logger.warning('启动 ' + str(size) + ' 个命令页面 ...')
+
+    new_cmdpage_element = "//*[@id='etoolbar_toolbarSection_newcommandpagebtn_id']"
+    wait_element(new_cmdpage_element, '新命令页按钮', 0)
+
+    for i in range(size) :
         time.sleep(1)
+        browser.find_element_by_xpath(new_cmdpage_element).click()
 
-        try:
-            cmdbutton = browser.find_element_by_xpath("//*[@id='etoolbar_toolbarSection_newcommandpagebtn_id']")
-        except NoSuchElementException as e:
-            print('等待加载主页面')
-            continue
-        break
+        cmdline_element = "//*[@id='cryptics" + str(i+1) + "_cmd_shellbridge_shellWindow_top_left_modeString_cmdPromptInput']"
+        wait_element(cmdline_element, '命令页'+str(i+1), 0)
 
-
-    print('主页面加载完成, 启动5个页面 ...')
-    time.sleep(2)
-    browser.find_element_by_xpath("//*[@id='cookiebanner_cookiebanner_cookieBannerAcceptButton']/button").click()
-    time.sleep(2)
-    browser.find_element_by_xpath("//*[@id='ngb-popover-1']/div[2]/div/button").click()
-    time.sleep(2)
-    for i in range(5) :
-        time.sleep(1)
-        cmdbutton.click()
-
-        while True :
-            time.sleep(1)
-
-            try:
-                xpath = "//*[@id=cryptics'" + str(i) + "_cmd_shellbridge_shellWindow_top_left_modeString_cmdPromptInput']"
-                browser.find_element_by_xpath(xpath)
-            except NoSuchElementException as e:
-                print('等待加载' + str(i) + '命令页面')
-                continue
-            break
-
-        print('命令页面' + str(i) + '加载完成')
-
-    print('全部命令页面加载完成')
+    logger.warning('全部命令页面加载完成')
 
 
 
 def execute_instruction(page, cmd):
-    logger.warning("命令 = " + cmd)
-    js = 'var ucode = document.getElementById("cryptics' + str(page) + '_cmd_shellbridge_shellWindow_top_left_modeString_cmdPromptInput"); ucode.value=arguments[0]'
-    return browser.execute_script(js,cmd)
+    try :
+        page_element = '//*[@id="etaskmgr_taskBar"]/ul[2]/li['+ str(page+2) +']'
+        browser.find_element_by_xpath(page_element).click()
 
+        cmdline_element = "//*[@id='cryptics" + str(page) + "_cmd_shellbridge_shellWindow_top_left_modeString_cmdPromptInput']"
+        wait_element(cmdline_element, '命令页'+str(page), 0)
+        browser.find_element_by_xpath(cmdline_element).send_keys(cmd, Keys.ENTER)
 
-
-
-
+    except JavascriptException as e :
+        logger.warning('命令 = ' + cmd)
+        logger.warning('错误 = ' + str(e))
 
 if __name__ == '__main__':
 
     branch_size = base_config["branch_size"]
 
     login()
-    start_cmd_page()
+    start_cmd_page(branch_size)
     execute_instruction(1, "AN02DECLOSFRA/ALH")
+    execute_instruction(2, "AN02DECLOSFRA/ALH")
     #
     # book_list = []
     # for i in range(branch_size):
@@ -152,11 +134,4 @@ if __name__ == '__main__':
     #     pool.putRequest(req)
     #     time.sleep(1 / branch_size)
     # pool.wait()
-
-# //*[@id="cryptics1_cmd_shellbridge_shellWindow_top_left_modeString_cmdPromptInput"]
-# //*[@id="cryptics2_cmd_shellbridge_shellWindow_top_left_modeString_cmdPromptInput"]
-# //*[@id="cryptics3_cmd_shellbridge_shellWindow_top_left_modeString_cmdPromptInput"]
-
-browser.find_element_by_xpath("//*[@id='eusermanagement_logout_logo_logout_id']").click()
-browser.find_element_by_xpath("//*[@id='uicAlertBox_ok']").click()
 
