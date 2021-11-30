725ecefec107b2f558daadfd4acddc81aa303a4d
liruigang <yueyemingming@163.com>
2021-11-30 21:46:16 +0800

    修改日å日期到12/07

--- Galileo/book/mybook.py
+++ Galileo/book/mybook.py
@@ -26,7 +26,7 @@ if ret == False :
 def limit():
     # 2021-11-20 00:00 我是
     # pyinstaller.exe -F -p venv/Lib/site-packages/ mybook.py
-    if datetime.datetime.now() > datetime.datetime.strptime('2021-11-30 00:00', '%Y-%m-%d %H:%M'):
+    if datetime.datetime.now() > datetime.datetime.strptime('2021-12-07 00:00', '%Y-%m-%d %H:%M'):
         logger.warning("试用期限已到...")
         return True
     return False
