827037aba50bee14c826f618dc2b2a7e44e24055
mecoder <mecoder@163.com>
2022-05-20 10:53:48 +0800

    添加循环。

--- src/data_order.py
+++ src/data_order.py
@@ -3,36 +3,37 @@
 
 import json
 
-def get_payload(flight_offers):
-	data = \
-{
-	"data": {
-		"type": "flight-order",
-		"flightOffers": [
-			flight_offers
-		],
-		"travelers": [
-			{
-				"id": "1",
-				"dateOfBirth": "1985-10-17",
-				"name": {
-					"firstName": "Join",
-					"lastName": "Mustermann"
-				},
-				"gender": "MALE",
-				"contact": {
-					"emailAddress": "Join@Mustermann.com",
-					"phones": [
-						{
-							"deviceType": "MOBILE",
-							"countryCallingCode": "33",
-							"number": "480080075"
-						}
-					]
-				}
-			}
-		]
-	}
-}
 
-	return data
+def get_payload(flight_offers):
+    data = \
+        {
+            "data": {
+                "type": "flight-order",
+                "flightOffers": [
+                    flight_offers
+                ],
+                "travelers": [
+                    {
+                        "id": "1",
+                        "dateOfBirth": "1985-10-17",
+                        "name": {
+                            "firstName": "Join",
+                            "lastName": "Mustermann"
+                        },
+                        "gender": "MALE",
+                        "contact": {
+                            "emailAddress": "Join@Mustermann.com",
+                            "phones": [
+                                {
+                                    "deviceType": "MOBILE",
+                                    "countryCallingCode": "33",
+                                    "number": "480080075"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+
+    return data
