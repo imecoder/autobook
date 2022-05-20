722a078fd33dac8b7208c08b74e829373aa04e36
mecoder <mecoder@163.com>
2022-05-20 10:52:30 +0800

    添加循环订。

--- src/booking.py
+++ src/booking.py
@@ -16,7 +16,7 @@ ret, base_config = myfile.read_json('config.base.json')
 if ret == False:
 	exit(0)
 
-def get_access_token() :
+def do_get_token() :
 	url_token = 'https://travel.api.amadeus.com/v1/security/oauth2/token'
 
 	headers = {
@@ -42,7 +42,7 @@ def get_access_token() :
 	return True, access_token
 
 
-def do_order(access_token, ama_client_ref) :
+def do_order(access_token, ama_client_ref, payload) :
 
 	url = 'https://travel.api.amadeus.com/v1/booking/flight-orders'
 
@@ -52,20 +52,23 @@ def do_order(access_token, ama_client_ref) :
 		'ama-client-ref': ama_client_ref
 	}
 
-	import data_order
-	payload = data_order.get_payload()
-
 	ret, response = mynet.post(session=session, url=url, headers=headers, payload=json.dumps(payload))
 	if ret == False:
 		logger.warning(sys._getframe().f_code.co_name + ' 运行失败')
-		return False, ''
+		return False, ""
 
 
 	try:
 		jsonResponse = json.loads(response.text)
+
+		if 'errors' in jsonResponse :
+			if jsonResponse["errors"]['code'] == 38192 :
+				return False, '38192'
+
 		PNR = jsonResponse["data"]["associatedRecords"][0]["reference"]
 		logger.warning('PNR = ' + PNR)
 		return True, PNR
+
 	except json.decoder.JSONDecodeError as e:
 		logger.warning('解析json失败 : ' + str(e))
 		logger.warning(sys._getframe().f_code.co_name + ' 运行失败')
@@ -77,31 +80,41 @@ def do_order(access_token, ama_client_ref) :
 
 
 def main():
-	pass
-
-
-if __name__ == '__main__':
-
 	branch_size = base_config['branch_size']
 	print(branch_size)
 
-	if mylimit.limit() == True:
-		exit(0)
+	while True :
+		if mylimit.limit() == True:
+			exit(0)
 
-	ret, access_token = get_access_token()
-	if ret == False :
-		exit(-1)
+		ret, access_token = do_get_token()
+		if ret == False :
+			time.sleep(5)
+			continue
 
-	ama_client_ref = 'LOSN828UU-' + str(time.time())
-	# ama_client_ref = 'LOSN828UU-1649074792.4925225'
+		while True:
+			ama_client_ref = 'LOSN828UU-' + str(time.time())
+			# 获取制作订单信息
+			import data_order
+			payload = data_order.get_payload()
 
+			# 订票
+			ret, PNR = do_order(access_token, ama_client_ref, payload)
+			if ret == False :
+				if PNR == '38192' :
+					break
+				continue
 
-	ret, PNR = do_order(access_token, ama_client_ref)
-	if ret == False :
-		exit(-1)
+			# 此时订票成功已经成功的情况下
+			break
 
-
+		# 此时订票成功已经成功的情况下
+		break
 
 	logger.warning('程序退出 ...')
 	logger.warning('')
 	logger.warning('')
+
+
+if __name__ == '__main__':
+	main()
